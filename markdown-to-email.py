4e0954a57dc81ceb5b838923024ab01a8e34dbfb
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2020-02-14 18:49:01 +0100

    Add march news

--- markdown-to-email.py
+++ markdown-to-email.py
@@ -10,23 +10,14 @@ should be constructed as a plain-text file of the following format:
     ---
     Markdown content here
 
+@copyright https://gist.github.com/cleverdevil/8250082
+
 The script accepts content from stdin and, by default, prints the raw
 generated email content to stdout.
 
 Preview your message on OS X by invoking the script with `-p` or 
 `--preview`, and it will open in your default mail client.
 
-To send the message, invoke with `-s` or `--send`. You must have a
-JSON file in your home directory named `.markdown-to-email.json`
-with the following keys:
-
-    {
-        "username": "smtp-username", 
-        "smtp": "smtp.gmail.com:587", 
-        "password": "your-password" 
-    }
-
-Enjoy!
 '''
 
 
@@ -36,17 +27,19 @@ import json
 import argparse
 import smtplib
 import subprocess
+import datetime, locale
 
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
 
 try:
     import pygments
-    import markdown
+    import markdown as markdown
 except ImportError:
-    print 'This script requires pygements and markdown to be installed.'
-    print 'Please:'
-    print '   pip install pygments markdown'
+    print('This script requires pygements and markdown to be installed.')
+    print('Please:')
+    print('   pip install pygments markdown')
     sys.exit(0)
 
 # define arguments
@@ -55,53 +48,43 @@ parser = argparse.ArgumentParser(description='Format and send markdown-based ema
                                  epilog=__doc__)
 parser.add_argument('-p', '--preview', action='store_true', 
                     help='Preview the email in Apple Mail.')
-parser.add_argument('-s', '--send', action='store_true', 
-                    help='Send the email using your configuration.')
 args = parser.parse_args()
 
 # read in raw message content
-raw_content = sys.stdin.read()
-
-# split out the headers from the markdown message body
-header_content, markdown_content = raw_content.split('--', 1)
+content = sys.stdin.read()
 
 # render the markdown into HTML 
-css = subprocess.check_output(['pygmentize', '-S', 'default', '-f', 'html'])
-markdown_content = markdown_content.strip()
-html_content = markdown.markdown(markdown_content, ['extra', 'codehilite'])
-html_content = '<style type="text/css">'+css+'</style>'+html_content
+css = str(subprocess.check_output(['pygmentize', '-S', 'colorful', '-a', '.codehilite', '-f', 'html']))
+
+content = content.strip()
+html_content = markdown.markdown(content, extensions=['extra', 'codehilite', 'fenced_code'])
+print(html_content)
+html_content = '<style type="text/css">'+css+'</style>'+"<img src='three-balls.png'>"+html_content
 
 # create a multipart email message
 message = MIMEMultipart('alternative')
 
-# parse the headers
-headers = {}
-for line in header_content.strip().split('\n'):
-    if not line.strip(): continue
-    key, value = line.split(':', 1)
-    headers[key.strip()] = value.strip()
-
 # set the headers
-message['To'] = headers.get('To', '')
-message['From'] = headers.get('From', '')
-message['Subject'] = headers.get('Subject', 'No subject')
+message['To'] = "julia@services.cnrs.fr"
+message['From'] = "pierre.navaro@univ-rennes1.fr"
+
+today = datetime.datetime.now()
+locale.setlocale(locale.LC_ALL, 'fr_FR')
+month = today.strftime("%B")
+year = today.strftime("%Y")
+
+message['Subject'] = f"Nouvelles Julia {month} {year}"
+img_data = open("three-balls.png", 'rb').read()
+image = MIMEImage(img_data, name=os.path.basename("three-balls.png"))
 
 # attach the message parts
-message.attach(MIMEText(markdown_content, 'plain'))
+message.add_header('Content-Disposition', 'attachment', filename="three-balls.png")
+message.attach(image)
+message.attach(MIMEText(content, 'plain'))
 message.attach(MIMEText(html_content, 'html'))
 
-if args.send:
-    to = message['To'].split(', ')
-    
-    with open(os.path.expanduser('~/.markdown-to-email.json'), 'rb') as f:
-        config = json.loads(f.read())
-        server = smtplib.SMTP(config['smtp'])
-        server.starttls()
-        server.login(config['username'], config['password'])
-        server.sendmail(message['From'], to, message.as_string())
-        server.quit()
-elif args.preview:
+if args.preview:
     open('/tmp/preview.eml', 'w').write(message.as_string())
     os.system('open -a Mail /tmp/preview.eml')
 else:
-    print message.as_string()
+    print(message.as_string())
