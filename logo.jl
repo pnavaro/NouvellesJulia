f012355828f8b0f38735cd14d41e96e51fab26d2
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2021-09-29 18:08:30 +0200

    Premier jet de la lettre du mois d'octobre

---
+++ logo.jl
@@ -0,0 +1,39 @@
+using Luxor
+
+# @svg begin
+#     sethue(Luxor.julia_blue)
+#     setline(8)
+#     circle(Point(0,0), 200, :stroke)
+#     juliacircles()
+#     sethue("white")
+#     setfont("JuliaMono", 55)
+#     points = ngon(0, 0, 100, 3, pi/6, vertices=true)
+#     settext.(["f", "f", "uj"], points; markup=true, halign = "center", valign = "center")
+# 
+# 
+# end
+
+function cocarde(fname)
+    origin()
+
+    circle(O, 135, :clip)
+    sethue("white")
+    paint()
+
+    sethue("blue")
+    setline(10)
+    circle(O, 130, :stroke)
+    sethue("red")
+    circle(O, 110, :stroke)
+
+    sethue("black")
+    fontface("JuliaMono-ExtraBold")
+    fontsize(24)
+    text("Nouvelles", Point(0, -30), halign=:center, valign=:center)
+    translate(-50, -20)
+    scale(0.3)
+    julialogo()
+    finish()
+end
+
+cocarde("logo.png")
